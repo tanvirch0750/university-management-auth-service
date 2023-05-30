0e458e498dc8da93f0c3443a0594b5e572355449
tanvirch0750 <tanvir0750@yahoo.com>
2023-05-30 21:43:34 +0600

    implement create user service, create user controller & crate user route

---
+++ src/app/modules/users/users.utils.ts
@@ -0,0 +1,17 @@
+import User from './users.model';
+
+export const findLastUserId = async () => {
+  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean();
+
+  return lastUser?.id;
+};
+
+export const generateUserId = async () => {
+  const latestId = (await findLastUserId()) || (0).toString().padStart(5, '0');
+
+  // increment by one
+  const incrementedId = (parseInt(latestId) + 1).toString().padStart(5, '0');
+  return `${incrementedId}`;
+};
